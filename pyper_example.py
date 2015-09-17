8d5d4a71649db1cbf6ee8c5bd275724fa42661df
paulmueller <paul.mueller@biotec.tu-dresden.de>
2015-09-17 15:23:05 +0200

    find R manually

--- pyper_example.py
+++ pyper_example.py
@@ -5,13 +5,36 @@ A Python script that uses numpy and pyper.
 """
 from __future__ import division, print_function
 
+import os
 import numpy as np
 import pyper
+import sys
 
 xs = np.linspace(0,1,10)
 
+
+# additional path searches
+Rpaths = ["C:\\Program Files\\R\\R-3.2.2\\bin\\i386",
+               "C:\\Program Files\\R\\R-3.2.2\\bin\\i386",
+               "/usr/bin/"]
+
+Rexes = list()
+for binary in ["R", "R.exe"]:
+    Rexes += [ os.path.join(loc, binary) for loc in Rpaths ]
+
+# standard path search:
+Rexe = "R"
+
+# find other installation:
+for b in Rexes:
+    if os.path.exists(b):
+        Rexe = b
+        break
+print("Using R: ", Rexe)
+
+
 #Open a pyper instance
-r1 = pyper.R(use_pandas = True) 
+r1 = pyper.R(use_pandas = True, ) 
 r1.assign("xs", xs) 
 r1("DFRAME=data.frame(xs)")
 r1("result <- DFRAME[xs]*2")   
